4912377f2f8d5ed848edbbee7ce1365281f3a848
RANDRIANAINA Georges Aaron <georges-aaron.randrianaina@irisa.fr>
2022-04-20 16:48:28 +0200

    kconfig: cfconfig: fix of configfix-cli to fix on command line
    Build:
        make build_cfconfig
    Run:
        export SRCARCH=x86 LD=$(which ld) CC=$(which gcc) srctree=$(pwd) ;\
               scripts/kconfig/cfconfig Kconfig <option> <value>

--- scripts/kconfig/cfconfig.c
+++ scripts/kconfig/cfconfig.c
@@ -1,7 +1,3 @@
-// SPDX-License-Identifier: GPL-2.0
-/*
- * Copyright (C) 2021 Patrick Franz <deltaone@debian.org>
- */
 
 #define _GNU_SOURCE
 #include <assert.h>
@@ -16,72 +12,81 @@
 
 #include "configfix.h"
 
-static struct symbol * read_symbol_from_stdin(void);
 static struct symbol_dvalue * sym_create_sdv(struct symbol *sym, char *input);
-static void handle_fixes(struct sfl_list *diag);
-
+static void print_diagnoses_symbol(struct sfl_list *diag_sym);
 /* -------------------------------------- */
+
 
 int main(int argc, char *argv[])
 {
-	CFDEBUG = true;
+        struct sfl_list *diagnoses;
+        struct sdv_list *symbols;
+        char buf[256], *option = argv[2], *newval = argv[3];
+        const char *currval;
+        struct symbol *sym;
+        struct symbol_dvalue *sdv;
+        struct sfl_node *node;
+        unsigned int counter;
 
-	if (argc > 1 && !strcmp(argv[1], "-s")) {
-		printd("\nHello configfix!\n\n");
+        CFDEBUG = true;
 
-		run_satconf_cli(argv[2]);
-		return EXIT_SUCCESS;
-	}
+        printd("CONFIGFIX-CLI\n-------------\nModified for CMUT\n");
+        printd("Initialization: Kconfig, .config\n");
 
-	printd("\nCLI for configfix!\n");
+        conf_parse(argv[1]);
 
-	init_config(argv[1]);
+        if (conf_read(NULL)){
+          printd(".config not found!\n");
+          return EXIT_FAILURE;
+        }
 
-	struct sfl_list *diagnoses;
-	struct sdv_list *symbols;
+        symbols = sdv_list_init();
 
-	while(1) {
-		/* create the array */
-		symbols = sdv_list_init();
+        if ((sym = sym_find(option)) == NULL){
+                printd("Symbol %s not found\n", option);
+                return EXIT_FAILURE;
+        }
 
-		/* ask for user input */
-		struct symbol *sym = read_symbol_from_stdin();
+        sdv = sym_create_sdv(sym, newval);
+        currval = sym_get_string_value(sym);
 
-		printd("Found symbol %s, type %s\n\n", sym->name, sym_type_name(sym->type));
-		printd("Current value: %s\n", sym_get_string_value(sym));
-		printd("Desired value: ");
+        if (!(strcmp(currval, newval))){
+                printd("%s already set to %s\n", sym->name, newval);
+                return EXIT_SUCCESS;
+        }
 
-		char input[100];
-		fgets(input, 100, stdin);
-		strtok(input, "\n");
+        printd("Change: %s (%s) %s -> %s\n",
+               sym->name, sym_type_name(sym->type), currval, newval);
 
-		struct symbol_dvalue *sdv = sym_create_sdv(sym, input);
-		sdv_list_add(symbols, sdv);
+        /* sym_calc_value(sym); */
+        /* conf_write("___CONFIG_TEST"); */
+        /* return EXIT_SUCCESS; */
 
-		diagnoses = run_satconf(symbols);
-		handle_fixes(diagnoses);
-	}
+        sdv_list_add(symbols, sdv);
 
-	return EXIT_SUCCESS;
-}
+        diagnoses = run_satconf(symbols);
 
-/*
- * read a symbol name from stdin
- */
-static struct symbol * read_symbol_from_stdin(void)
-{
-	char input[100];
-	struct symbol *sym = NULL;
+        if (diagnoses->size == 0){
+                printd("No diagnoses available: everything is OK for the change\n");
+                printd("sym_calc_value: update symbol's new value\n");
+                sym_calc_value(sym);
+                sprintf(buf, "___config%s", option);
+                conf_write(buf);
+        } else {
+                /* print_diagnoses_symbol(diagnoses); */
+                counter = 1;
+                sfl_list_for_each(node, diagnoses){
+                        apply_fix(node->elem);
+                        print_diagnosis_symbol(node->elem);
+                        /* printd("\nResetting config.\n"); */
+                        sprintf(buf, "___config%s-%d", option, counter++);
+                        conf_write(buf);
+                        conf_read(NULL);
+                }
 
-	printd("\n");
-	while (sym == NULL) {
-		printd("Enter symbol name: ");
-		fgets(input, 100, stdin);
-		strtok(input, "\n");
-		sym = sym_find(input);
-	}
+        }
 
-	return sym;
+        return EXIT_SUCCESS;
 }
 
 /*
@@ -117,60 +122,13 @@ static struct symbol_dvalue * sym_create_sdv(struct symbol *sym, char *input)
 /*
  * print the diagnoses of type symbol_fix
  */
-static void print_diagnoses_symbol(struct sfl_list *diag_sym)
-{
-	struct sfl_node *arr;
-	unsigned int i = 1;
+/* static void print_diagnoses_symbol(struct sfl_list *diag_sym) */
+/* { */
+/* 	struct sfl_node *arr; */
+/* 	unsigned int i = 1; */
 
-	sfl_list_for_each(arr, diag_sym) {
-		printd(" %d: ", i++);
-		print_diagnosis_symbol(arr->elem);
-	}
-}
-
-static void apply_all_adiagnoses(struct sfl_list *diag) {
-	printd("Applying all diagnoses now...\n");
-
-	unsigned int counter = 1;
-	struct sfl_node *node;
-	sfl_list_for_each(node, diag) {
-		printd("\nDiagnosis %d:\n", counter++);
-		apply_fix(node->elem);
-
-		printd("\nResetting config.\n");
-		conf_read(NULL);
-	}
-}
-
-/*
- * print all void print_fixes()
- */
-static void handle_fixes(struct sfl_list *diag)
-{
-	printd("=== GENERATED DIAGNOSES ===\n");
-	printd("-1: No changes wanted\n");
-	printd(" 0: Apply all diagnoses\n");
-	print_diagnoses_symbol(diag);
-
-	int choice;
-	printd("\n> Choose option: ");
-	scanf("%d", &choice);
-
-	if (choice == -1 || choice > diag->size)
-		return;
-
-	if (choice == 0) {
-		apply_all_adiagnoses(diag);
-		return;
-	}
-
-	unsigned int counter;
-	struct sfl_node *node = diag->head;
-	for (counter = 1; counter < choice; counter++)
-		node = node->next;
-
-	apply_fix(node->elem);
-
-	printd("\nResetting config.\n");
-	conf_read(NULL);
-}
+/* 	sfl_list_for_each(arr, diag_sym) { */
+/* 		printd(" %d: ", i++); */
+/* 		print_diagnosis_symbol(arr->elem); */
+/* 	} */
+/* } */
